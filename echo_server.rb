67e61a32e33f27425a23d6bbf42c708618d9a5e8
Linh Nguyen My <lnguyenmy1@gmail.com>
2016-11-22 23:32:14 +0000

    Injected dependencies

--- echo_server.rb
+++ echo_server.rb
@@ -1,45 +1,44 @@
 class UserEcho
 
-	def initialize(welcome, instructions)
-		@welcome = "Welcome!"
-		@instructions = "Please enter a word to be reprinted, to exit this program please enter exit:"
-	end
+  def initialize
+    print_welcome_message
+    print_instruction_message
+    start
+  end
 
-	def welcome_message
-		@welcome
-	end
+  def print_welcome_message
+    print "Welcome!\n"
+  end
 
-	def instruction_message
-		@instructions
-	end
+  def print_instruction_message
+    print "Please enter a word to be reprinted, to exit this program please enter exit:\n"
+  end
 
-	def goodbye_message
-		"Goodbye!"
-	end
+  def print_goodbye_message
+    print "Goodbye!"
+  end
 
-	private
+  private
 
-	def start
-		@welcome
-		@instructions
-		input
-	end
+  def start
+    input
+  end
 
-	def input
-		@@user_input = STDIN.readline.chomp
-		exit_entered?
-	end
+  def input
+    @user_input = STDIN.readline.chomp
+    exit_entered?
+  end
 
-	def exit
-		@user_input == "exit"
-	end
+  def exit
+    @user_input == "exit"
+  end
 
-	def exit_entered?
-		while exit == true
-			goodbye_message
-		else 
-			input
-		end
-	end
+  def exit_entered?
+    while exit == true
+      print_goodbye_message
+    else 
+      input
+    end
+  end
 
 end
